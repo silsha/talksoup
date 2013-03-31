70313bf31bce9828834a8bbe18d731fceedc9f3e
Felix Arndt <mail@felixarndt.de>
2013-04-01 00:54:11 +0200

    mongodb workaround. will fix later™

--- application/config/mongodb.php
+++ application/config/mongodb.php
@@ -17,12 +17,12 @@
  */
 
 $config['default']['mongo_hostbase'] = 'localhost:27017';
-$config['default']['mongo_database'] = '';
+$config['default']['mongo_database'] = 'talksoup';
 $config['default']['mongo_username'] = '';
 $config['default']['mongo_password'] = '';
-$config['default']['mongo_persist']  = TRUE;
+$config['default']['mongo_persist']  = FALSE;
 $config['default']['mongo_persist_key']	 = 'ci_persist';
 $config['default']['mongo_replica_set']  = FALSE;
 $config['default']['mongo_query_safety'] = 'safe';
-$config['default']['mongo_suppress_connect_error'] = TRUE;
+$config['default']['mongo_suppress_connect_error'] = FALSE;
 $config['default']['mongo_host_db_flag']   = FALSE;
