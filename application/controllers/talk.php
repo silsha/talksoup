60b5aa1127bb331952c719b0d4fc969f59511a56
Felix Arndt <mail@felixarndt.de>
2013-04-01 17:08:05 +0200

    more template demodata

--- application/controllers/talk.php
+++ application/controllers/talk.php
@@ -4,6 +4,20 @@ class Talk extends CI_Controller {
 	public function index()
 	{
 		$this->smarty->assign('lang_Welcome', 'Talks');
+		$this->smarty->assign('videos', 
+			array(
+				array(
+					'title'		=> 'Mein toller Vortrag',
+					'lenght'	=> '1234',
+					'speaker'	=> 'silsha' 
+					),
+				array(
+					'title'		=> 'Mein tollerer Vortrag',
+					'lenght'	=> '2345',
+					'speaker'	=> 'l3kn' 
+					)
+			));
+
 		$this->smarty->assign('content', $this->smarty->fetch('talks/index.tpl'));
 		$this->smarty->view('template');
 	}
--- application/views/templates/talks/index.tpl
+++ application/views/templates/talks/index.tpl
@@ -1,1 +1,6 @@
 <h2>{$lang_Welcome}</h2>
+<ul>
+	{foreach $videos as $video}
+	<li>{$video.title} by {$video.speaker}</li>
+	{/foreach}
+</ul>
